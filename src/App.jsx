2b7a606024ee7a3232feb6f1ac61262c0a63dd1d
ckiroshan <ckiroshan@gmail.com>
2025-08-10 19:45:43 +0530

    feat: configure shadcn with button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div className="container">
         <h1 className="text-red-700">This site is working perfectly 😉!</h1>
+        <Button variant="default" className="text-lg px-8 py-3 shadow-lg bg-amber-300">
+          Get Started
+        </Button>
       </div>
     </>
   );
