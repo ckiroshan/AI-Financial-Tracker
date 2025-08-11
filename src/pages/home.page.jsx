90da06e486c071dc3e3b73d420c1aba70bbc9d82
ckiroshan <ckiroshan@gmail.com>
2025-08-11 20:29:43 +0530

    feat: add Home page & configure route

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="container mx-auto flex px-4 sm:px-6 md:mx-20 lg:mx-30 lg:px-8 py-4">
+      <h1 className="font-bold text-xl">This is the Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
