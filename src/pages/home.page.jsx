691fe134a7aec0664210c0fad7df0a2a9d9abe46
yalindi <yalindidasanya@gmail.com>
2025-08-14 21:35:24 +0530

    feat: add the newest features to the landing page

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,5 +1,8 @@
 import Hero from '../components/home/hero'
 import Contact from '../components/home/contact'
+import AboutUs from '../components/home/about-us'
+import ProblemSolution from '@/components/home/problem-solution';
+import Features from '@/components/home/features';
 
 
 const HomePage = () => {
@@ -7,6 +10,9 @@ const HomePage = () => {
     <>
     <div className="container mx-auto px-4 sm:px-6 lg:px-32 py-4">  
       <Hero/>
+      <ProblemSolution/>
+      <Features/>
+      <AboutUs/>
       <Contact/>
       
     </div>
