87cc290535c3cd4ed1542750622801bf99ae617c
ckiroshan <ckiroshan@gmail.com>
2025-08-14 12:42:56 +0530

    feat: add Demo Home & Dashboard pages

---
+++ src/pages/dashboard.page.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="rounded-lg mx-2 md:mx-20 lg:mx-30 my-2 py-4 px-8 bg-primary-foreground">
+      <h1 className="font-bold text-xl">This is the User Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
