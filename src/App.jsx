ee662ff63420e006c65ce0951633c715315b3edb
ckiroshan <ckiroshan@gmail.com>
2025-08-17 15:30:39 +0530

    feat: centralize App authentication logic with a protected layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import RootLayout from "./components/layouts/root-layout.page";
 import HomePage from "./pages/home.page";
 import Dashboard from "./pages/dashboard.page";
 import SignInPage from "./pages/sign-in.page";
 import SignUpPage from "./pages/sign-up.page";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import ProtectLayout from "./components/layouts/protect.layout";
+import { useUser } from "@clerk/clerk-react";
+
+const IndexRouteComponent = () => {
+  // Check if user is loaded & signed in
+  const { isLoaded, isSignedIn } = useUser();
+  if (!isLoaded) return null;
+  if (isSignedIn) return <Navigate to="/user/dashboard" />;
+  // If not signed in, return the home page
+  return <HomePage />;
+};
 
 function App() {
   return (
@@ -13,24 +23,14 @@ function App() {
       <Routes>
         <Route path="/" element={<RootLayout />}>
           {/* Public Routes */}
-          <Route index element={
-              <>
-                <SignedIn><Dashboard /></SignedIn>
-                <SignedOut><HomePage /></SignedOut>
-              </>
-            }
-          />
-          {/* Signed In Dashboard Route */}
-          <Route path="/user/dashboard" element={
-              <>
-                <SignedIn><Dashboard /></SignedIn>
-                <SignedOut><SignInPage /></SignedOut>
-              </>
-            }
-          />
+          <Route index element={<IndexRouteComponent />} />
           {/* Authentication Routes */}
           <Route path="login" element={<SignInPage />} />
           <Route path="register" element={<SignUpPage />} />
+          {/* Protected Routes */}
+          <Route element={<ProtectLayout />}>
+            <Route path="/user/dashboard" element={<Dashboard />} />
+          </Route>
         </Route>
       </Routes>
     </>
